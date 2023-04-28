362e80590546ca2f0459c6f821511941b33f764f
ijp <isaacpeterson2017@gmail.com>
2023-04-28 14:53:34 -0500

    the freecad export script now has a working tui and is able to recurse subdirectories and build a parallel file structure in the output directory

--- src/export.py
+++ src/export.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
 import argparse, subprocess, os
 
+freecad_extention = '.FCStd'
+stl_extention = '.stl'
+
 def export(input, output):
+    if not output.endswith(stl_extention): 
+        output = output.split('.')[0] + stl_extention
+
     if args.verbose: print("exporting " + input + " as " + output)
     freecad_args = ["/home/ijp/thundertoad/hardware/src/freecad_export_stl.py", input, output]
     result = subprocess.run(freecad_args, stdout=subprocess.PIPE)
@@ -9,47 +15,30 @@ def export(input, output):
         print(result.stdout.decode())
 
 
-def find_em(relative_path):
-    if args.verbose: print("searching " + relative_path)
-    getit = args.source + relative_path
-    if '.' in relative_path: [relative_path, extention] = relative_path.split('.')
-    elif '.' in args.source: [args.source, extention] = args.source.split('.')
-    sndit = args.dest + relative_path + '.stl'
-
-    if args.verbose: print('exporting' + getit + 'as' + sndit)
-
-    if not os.path.exists(sndit): open(sndit, 'x').close()
-    elif args.verbose: print("overwriting" + sndit)
-    
-    
-    if args.verbose: print(getit + ">>>--->" + sndit)
-
-    if extention=='FCStd': export(getit, sndit)
-    elif os.path.isdir(getit):
+def find_em(source, dest):
+    if source.endswith(freecad_extention): export(source, dest)
+    elif os.path.isdir(source):
         if args.recursive:
-            for child in os.listdir(getit): find_em(relative_path + child)
-        else: print(getit, "is a directory. Use -r")
-    elif args.verbose:
-        print(getit + " doesn't look like a freecad model")
+            for child in os.listdir(source): 
+                find_em(os.path.join(source, child), os.path.join(dest, child))
+        else: print(source, "is a directory. Use -r")
+    elif args.verbose: print(source + " is not a freecad file or directory")
 
 
 parser = argparse.ArgumentParser(description='creates STLs from freecad files')
-parser.add_argument('source', type=str, help='input file')
-parser.add_argument('dest', type=str, nargs='?', help='output file or dir')
+parser.add_argument('source', type=str, help='input file or dir')
+parser.add_argument('dest', type=str, nargs='?', help='output dir')
 parser.add_argument('-v', '--verbose', help='more output', action='store_true')
 parser.add_argument('-r', '--recursive', help='export children', action='store_true')
 args = parser.parse_args()
 
+if args.dest == None : args.dest = os.getcwd()
+
 args.source = os.path.abspath(args.source)
+args.dest = os.path.abspath(args.dest)
 
-if args.dest==None:
-    args.dest = args.source.split('.')[0]
-elif os.path.isfile(args.dest):
-    if args.recursive: print("-r detected " + args.dest + " must be a directory")
-    elif os.path.isdir(args.source): print("cannot batch export into a single file")
-    elif not args.dest.endswith('.stl'): print(args.dest + "must end with .stl")
-    else: 
-        export(args.source, args.dest)
-        exit(0)
 
-find_em("")
+if(args.source.endswith(freecad_extention)): 
+    export(args.source, os.path.join(args.dest, os.path.basename(args.source)))
+
+else: find_em(args.source, args.dest)
--- src/test.sh
+++
@@ -1,4 +0,0 @@
-#./export.py /home/ijp/thundertoad/hardware/freecad/unibody.FCStd \
-#            /home/ijp/thundertoad/hardware/stl/output.stl
-
-./export.py /home/ijp/thundertoad/hardware/freecad -r
